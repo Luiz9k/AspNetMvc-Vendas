a0d135c7161e4e280d4596c1493a73f04f0b953f
Luiz9k <luiz.xaxim.95@gmail.com>
2020-04-27 21:16:03 -0300

    SellerService and basic FindAll

---
+++ Vendas/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vendas.Models;
+
+namespace Vendas.Services
+{
+    public class SellerService
+    {
+        private readonly VendasContext _context;
+
+        public SellerService(VendasContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            //acessa a fonte de dados de vendedores, e converter para uma lista
+            return _context.Seller.ToList();
+        }
+    }
+}
