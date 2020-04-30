f522d791bbcc9dc128f451825ab95e61fc4cc246
Luiz9k <luiz.xaxim.95@gmail.com>
2020-04-29 22:42:22 -0300

    Update seller and custom service exception

--- Vendas/Controllers/SellersController.cs
+++ Vendas/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Vendas.Models;
 using Vendas.Services;
 using Vendas.Models.ViewModels;
+using Vendas.Services.Exceptions;
 
 namespace Vendas.Controllers
 {
@@ -81,6 +82,46 @@ namespace Vendas.Controllers
             return View(obj);
         }
 
+        public IActionResult Edit(int? id)
+        {
+            if ( id == null)
+            {
+                return NotFound();
+            }
 
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments};
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if ( id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundExcepetion)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+            
+        }
     }
 }
---
+++ Vendas/Services/Exceptions/NotFoundExcepetion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vendas.Services.Exceptions
+{
+    public class NotFoundExcepetion : ApplicationException
+    {
+        public NotFoundExcepetion(string message) : base(message)
+        {
+
+        }
+    }
+}
